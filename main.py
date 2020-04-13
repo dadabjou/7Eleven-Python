ac7ceffcd74072ae5d23a500156b33a0895d21cd
Daryoush <daryadab@gmail.com>
2020-04-14 00:46:41 +1000

    android autostart

--- main.py
+++ main.py
@@ -492,6 +492,11 @@ if __name__ == '__main__':
     scheduler.add_job(autolocker.start_lockin, 'interval', seconds=1800)
     scheduler.start()
 
+    droid = Android()
+    uri2open = 'http://0.0.0.0:5000'
+    intent2start = droid.makeIntent("android.intent.action.VIEW", uri2open, "text/html", None, [u"android.intent.category.BROWSABLE"], None, None, None)
+    print(droid.startActivityForResultIntent(intent2start.result))
+
     app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0')
 
